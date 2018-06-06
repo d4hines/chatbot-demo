19e041c73e7795efd6cf44f937fa2c06670c6ac4
David Tippett <dtippett@regent.edu>
2018-06-06 12:00:12 -0400

    broke out individual functionality into functions

--- examples/chatbot/src/chatbot/core.clj
+++ examples/chatbot/src/chatbot/core.clj
@@ -1,11 +1,30 @@
 (ns chatbot.core
   (:gen-class)
-  (:require [gniazdo.core :as ws]
+  (:require [gniazdo.core :as ws]))
    ;; [pneumatic-tubes.core :as tubes]
-   ))
+
+(def url "ws://localhost:3449/chat?name=chatbot&room=test")
+
+(defn handle-message [data] 
+  (prn 'received data))
+    
+(defn send-message [message] 
+      (ws/send-msg socket (str "[\"~:post-message\",\"" message "\"]")))
+
+(def message-generator
+  (send-message "Do I work now?"))
+
 (def socket
   (ws/connect
-   "ws://localhost:3449/chat?name=chatbot&room=test"
-   :on-receive #(prn 'received %)))
-(ws/send-msg socket "[\"~:post-message\",\"Hi, I'm Chatbot\"]")
+   url
+   :on-receive #(handle-message %))) ;;prn 'received %
 
+(def close-socket 
+  (ws/close socket))
+            
+            
+                                    
+ 
+  
+  
+  
